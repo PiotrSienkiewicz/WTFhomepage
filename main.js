563e1b1d8e824282d1f68479607d70954dfc3914
Piotr Sienkiewicz <suniciun@piasta.pl>
2020-03-14 18:24:36 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('siema');
+const name = 'Piotr'
+const age = 29
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age}`);
+
+console.log ('siema siema ziomek ❤')
